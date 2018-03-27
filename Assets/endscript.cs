bd3455ec73dc3abe91a2246c6b6df2e98220e13e
Imani Hunter <ihunter1@student.fitchburgstate.edu>
2018-03-27 11:06:29 -0400

    Added Imani's Project
    deleted empty folder, added in whatever

---
+++ Assets/endscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class endscript : MonoBehaviour
+{
+    // how to automatically create triggers around objects after the first one is interacted with
+
+    public Text directions;
+    public string endmessage;
+    private string trigger;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+        if (trigger == "Player" && Input.GetButtonDown("Fire1"))
+        {
+
+            directions.text = endmessage; // will not replace directions to "the end"
+        }
+    }
+    void OnTriggerEnter(Collider other)
+
+    {
+        trigger = other.tag;
+
+    }
+}
