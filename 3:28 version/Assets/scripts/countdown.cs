fbeb41ede3b81c46c70191eb4026f0458df3354a
Matthew Gendason <matthewgendason@gmail.com>
2018-03-28 20:26:29 -0400

    3/28 Version
    This is the most recent version

---
+++ 3:28 version/Assets/scripts/countdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countdown : MonoBehaviour
+{
+    private float timeRemaining;
+    public Text timer;
+
+    void Start()
+    {
+        timeRemaining = 300;
+    }
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining > 0)
+        {
+            float minutes = Mathf.Floor(timeRemaining / 60);
+            float seconds = Mathf.Floor(timeRemaining % 60);
+            timer.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+        }
+    }
+}
---
+++ 3:28 version/Assets/scripts/puzzletriggers.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class puzzletriggers : MonoBehaviour
+{
+
+    public Text directions;
+    public string message;
+    private string trigger;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+        if (trigger == "Player" && Input.GetButtonDown("Fire1"))
+        {
+            directions.text = message;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+
+    {
+        trigger = other.tag;
+
+    }
+}
+
+   
